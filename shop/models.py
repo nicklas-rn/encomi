7e51d086c388ab8579c2428ea0680538d5f7eafc
nicklas-rn <nicklas.rondot@gmail.com>
2022-07-27 15:28:52 +0200

    changed image upload folder

--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to="media/item_images")
+    image = models.ImageField(upload_to="item_images")
 
     def __str__(self):
         return self.image.name
