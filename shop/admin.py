78b218cd50cb87e588c7ded08bac969734da73f2
nicklas-rn <nicklas.rondot@gmail.com>
2022-07-27 15:02:49 +0200

    Images added

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Image
 
 
 admin.site.register(Item)
+admin.site.register(Image)
--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
+class Image(models.Model):
+    image = models.ImageField(upload_to="media/item_images")
+
+    def __str__(self):
+        return self.image.name
+
+
 class Item(models.Model):
     title = models.CharField(max_length=30)
     keywords = models.CharField(blank=True, null=True, max_length=120)
+    images = models.ManyToManyField(Image)
 
     def __str__(self):
         return self.title
