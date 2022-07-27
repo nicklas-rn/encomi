a28c3f35795d9667d573d33b86367e3c58574a86
nicklas-rn <nicklas.rondot@gmail.com>
2022-07-27 21:26:57 +0200

    solved bug in cart logic and change to linking of models

--- shop/models.py
+++ shop/models.py
@@ -3,14 +3,15 @@ from django.db import models
 
 class Image(models.Model):
     image = models.ImageField(upload_to="item_images")
+    item = models.ForeignKey('Item', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.image.name
 
 
-class Style(models.Model):
+class Item(models.Model):
     title = models.CharField(max_length=30)
-    price = models.FloatField()
+    keywords = models.CharField(blank=True, null=True, max_length=120)
 
     def __str__(self):
         return self.title
@@ -18,18 +19,17 @@ class Style(models.Model):
 
 class StyleGroup(models.Model):
     type = models.CharField(max_length=30)
-    styles = models.ManyToManyField(Style)
+    item = models.ForeignKey('Item', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.type
 
 
-class Item(models.Model):
+class Style(models.Model):
     title = models.CharField(max_length=30)
-    keywords = models.CharField(blank=True, null=True, max_length=120)
-    images = models.ManyToManyField(Image)
-    style_groups = models.ManyToManyField(StyleGroup)
-    price = models.FloatField(null=True, blank=True)
+    price = models.FloatField()
+    style_group = models.ForeignKey('StyleGroup', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
